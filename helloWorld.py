a41f8e9ba2a818fd2bd07d3b55b5744e3bffb91e
unknown <francesca.canale.95@gmail.com>
2019-12-16 16:06:40 +0100

    Cleaned code and added section reader

--- helloWorld.py
+++ helloWorld.py
@@ -3,85 +3,96 @@ from naoqi import ALProxy
 import wikipedia
 import unicodedata # To convert unicode (read from wikipedia) to string
 import time
+from prova import Recognizer
 import sys
 import functools
 import argparse
 
-import importlib
-importlib.import_module("prova.py")
 
-#_______________________________________________________________________________________________________________
-# API : http://doc.aldebaran.com/2-5/naoqi/index.html
+IP = "130.251.13.158"
 
-#Manage audio inputs and outputs, it is used by all other audio modules
-
-audio_module = ALProxy("ALAudioDevice", "130.251.13.158", 9559)
-mem_module = ALProxy("ALMemory", "130.251.13.158", 9559)
-# mem_module.setLanguage("English")  # Set the language
+# Manage audio inputs and outputs, it is used by all other audio modules
+audio_module = ALProxy("ALAudioDevice", IP, 9559)
+mem_module = ALProxy("ALMemory", IP, 9559)
 
 # Connects to speech proxy to make the robot speak
-speak_module = ALProxy("ALTextToSpeech", "130.251.13.158", 9559)
+speak_module = ALProxy("ALTextToSpeech", IP, 9559)
 
 # Connection to ALAnimatedSpeech proxy: it can create more lively speech by annotating the text yourself with some instructions or by adding animations for some words with ALSpeakingMovement
-animated_module = ALProxy("ALAnimatedSpeech", "130.251.13.158", 9559)
+animated_module = ALProxy("ALAnimatedSpeech", IP, 9559)
 
 # Connects to movement service to give the autonomous ability while speaking(enabled by default)
-#movement_module = ALProxy("ALSpeakingMovement", "130.251.13.158", 9559)
+# movement_module = ALProxy("ALSpeakingMovement", IP, 9559)
 
 # Connects to tablet proxy to display image
-tablet_module = ALProxy("ALTabletService", "130.251.13.158", 9559)
+tablet_module = ALProxy("ALTabletService", IP, 9559)
 
 # Connect to dialogue proxy, we need to create and upload a .top file
-#http://doc.aldebaran.com/2-5/naoqi/interaction/dialog/aldialog-api.html#aldialog-api
-dialogue_module = ALProxy("ALDialog", "130.251.13.158", 9559)
+dialogue_module = ALProxy("ALDialog", IP, 9559)
 dialogue_module.setLanguage("English")  # Set the language
 
 # Connects to speech proxy to make the robot understand what a human says
-understand_module = ALProxy("ALSpeechRecognition", "130.251.13.158", 9559)
+understand_module = ALProxy("ALSpeechRecognition", IP, 9559)
 understand_module.setLanguage("English")  # Set the language
-#____________________________________________________________________________________________________
-# For now we'll assume to have the keyword ready to be used to extract the info from wikipedia
 
-# Start the speech recognition engine with user Test_ASR
-#understand_module.subscribe("Test_ASR")
-#speak_module.say("Speech recognition engine started")
-#time.sleep(10)
-#understand_module.unsubscribe("Test_ASR")
+# Define the keyword
+keyword = "Meatball"
+vocabulary = ["yes", "no"]
+vocabulary.append(keyword)
 
-#if(speak_module.SpeechDetected == 0)
-#   keyword = ("understand_module.WordRecognized[0] wikipedia")
-#____________________________________________________________________
-keyword = ("Meatball")
-got_keyword = ("Not Meatball")
+# Load and access data from full Wikipedia pages
+ny = wikipedia.page(keyword)
+imageStr = unicodedata.normalize('NFKD', ny.images[1]).encode('ascii','ignore') # To get the image
+content = unicodedata.normalize('NFKD', wikipedia.summary(keyword, sentences=1)).encode('ascii','ignore') # To read the first phrase
+sections = wikipedia.sections(keyword)  # To get the name of the sections
 
-ny = wikipedia.page(keyword) #to load and access data from full Wikipedia pages
-
-speak_module.say("Keyword")
+# Add the sections into the vocabulary
+vocabulary.append(sections)
+understand_module.setVocabulary(vocabulary, False)
 speak_module.say(keyword)
 
 # Display the image on the tablet
-imageStr = unicodedata.normalize('NFKD', ny.images[1]).encode('ascii','ignore')
 tablet_module.showImage(imageStr)
 time.sleep(1)
 tablet_module.hideImage()
 
-sections = wikipedia.sections(keyword)
-
-# Convert the output of the wikipedia toolbox from unicode to string
-#content = unicodedata.normalize('NFKD', ny.content).encode('ascii','ignore') #to read all the page??
-content = unicodedata.normalize('NFKD', wikipedia.summary(keyword, sentences=1)).encode('ascii','ignore') #to read the first phrase
-#speak_module.say(content)
-#Try maybe it will be also animated!!!
-#animated_module.say(content)
-#if it works maybe is better to use this service especially during long interactions
+# Say the summary
+# speak_module.say(content)
 
 time.sleep(1)
 
-# # Example: Adds "yes", "no" and "please" to the vocabulary (without wordspotting)
-# vocabulary = ["yes", "no"]
-# understand_module.pause(True)
-# understand_module.setVocabulary(vocabulary, False)
-# understand_module.pause(False)
+# Starting the recognizer
+rec = Recognizer(IP)
+
+while True:
+    speak_module.say("Do you want more information?")
+
+    # Listening for the answer
+    user_input = rec.listen()
+    rec.cleanMemory()
+    print user_input
+
+    if user_input == "yes":
+        speak_module.say("Great! Which one of the following topic would you like to know more about?")
+        speak_module.say(sections)
+
+        # Listening for the answer
+        user_input_section = rec.listen()
+        rec.cleanMemory()
+        print user_input_section
+
+        section_summary = unicodedata.normalize('NFKD', wikipedia.summary(wikipedia.page(keyword).section(user_input_section), sentences=1)).encode('ascii','ignore')
+        break
+
+
+    elif user_input == "no":
+        speak_module.say("Ok.")
+        break
+
+    else:
+        speak_module.say("I'm sorry I didn't get that. Please answer yes or no.")
+        pass
+
 
 # understand_module.subscribe("WordRecognized")
 # # speechRecognized = mem_module.subscriber("WordRecognized")
@@ -108,59 +119,3 @@ time.sleep(1)
 #         got_keyword = value[0][6:-6]
 
 # id_speechRecognized = speechRecognized.signal.connect(functools.partial(onSpeechRecognized, "WordRecognized"))
-
-IP = "130.251.13.158"
-rec = prova.Recognizer(IP)
-
-word = rec.listen()
-rec.cleanMemory()
-print word
-
-#if(got_keyword != "Not Meatball"):
- #   print(got_keyword)
-
-'''
-    #if(SpeechDetected == 1):
-    got_keyword = understand_module.value
-    speak_module.say("Speech detected")
-    print(got_keyword)
-    understand_module.unsubscribe("Test_ASR")
-    speak_module.say("Speech recognition stopped")
-'''
-
-
-# sections = wikipedia.sections(keyword)
-
-
-# Writing topics' qichat code as text strings (end-of-line characters are important!)
-# dialogue_module.setLanguage("English")
-# topicContent = ('topic: ~mytopic()\n'
-#                 'language: enu\n'
-#                 'u: (Hello) Hello human!'
-#                 'proposal: Do you want to know more information?\n'
-#                 'u1: (yes) Great!\n'
-#                 'u2: (no) Ok.\n')
-# topicName = dialogue_module.loadTopicContent(topicContent)
-# print topicName
-# print dialogue_module.getLoadedTopics("English")
-
-# Activating the loaded topics
-# dialogue_module.activateTopic(topicName)
-
-# Starting the dialog engine - we need to type an arbitrary string as the identifier
-# We subscribe only ONCE, regardless of the number of topics we have activated
-# dialogue_module.subscribe('my_dialog_example')
-#
-#
-# try:
-#         raw_input("\nSpeak to the robot using rules from the just loaded .top file. Press Enter when finished:")
-# finally:
-#         # Stopping the dialog engine
-#         dialogue_module.unsubscribe('my_dialog_example')
-#
-#         # Deactivating the topic
-#         dialogue_module.deactivateTopic(topicName)
-#
-#         # Now that the dialog engine is stopped and there are no more activated topics,
-#         # we can unload our topic and free the associated memory
-#         dialogue_module.unloadTopic(topicName)
