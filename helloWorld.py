4e5ee9852273420433dcdb00590e0cc283b90c00
Alberto <alberto.ghiotto@hotmail.it>
2020-04-02 17:23:19 +0200

    Commented pepper side, working only on textual chatbot with user raw input

--- helloWorld.py
+++ helloWorld.py
@@ -10,28 +10,28 @@ import functools
 import argparse
 
 #IP = "130.251.13.158"
-IP = "127.0.0.1"
-port = 61972
-
-# Manage audio inputs and outputs, it is used by all other audio modules
-#audio_module = ALProxy("ALAudioDevice", IP, port)
-mem_module = ALProxy("ALMemory", IP, port)
-
-# Connects to speech proxy to make the robot speak
-speak_module = ALProxy("ALTextToSpeech", IP, port)
-
-# Connection to ALAnimatedSpeech proxy: it can create more lively speech by annotating the text yourself with some instructions or by adding animations for some words with ALSpeakingMovement
-animated_module = ALProxy("ALAnimatedSpeech", IP, port)
-
-# Connects to movement service to give the autonomous ability while speaking(enabled by default)
-# movement_module = ALProxy("ALSpeakingMovement", IP, port)
-
-# Connects to tablet proxy to display image
-#tablet_module = ALProxy("ALTabletService", IP, port)
-
-# Connect to dialogue proxy, we need to create and upload a .top file
-dialogue_module = ALProxy("ALDialog", IP, port)
-dialogue_module.setLanguage("English")  # Set the language
+# IP = "127.0.0.1"
+# port = 61972
+#
+# # Manage audio inputs and outputs, it is used by all other audio modules
+# #audio_module = ALProxy("ALAudioDevice", IP, port)
+# mem_module = ALProxy("ALMemory", IP, port)
+#
+# # Connects to speech proxy to make the robot speak
+# speak_module = ALProxy("ALTextToSpeech", IP, port)
+#
+# # Connection to ALAnimatedSpeech proxy: it can create more lively speech by annotating the text yourself with some instructions or by adding animations for some words with ALSpeakingMovement
+# animated_module = ALProxy("ALAnimatedSpeech", IP, port)
+#
+# # Connects to movement service to give the autonomous ability while speaking(enabled by default)
+# # movement_module = ALProxy("ALSpeakingMovement", IP, port)
+#
+# # Connects to tablet proxy to display image
+# #tablet_module = ALProxy("ALTabletService", IP, port)
+#
+# # Connect to dialogue proxy, we need to create and upload a .top file
+# dialogue_module = ALProxy("ALDialog", IP, port)
+# dialogue_module.setLanguage("English")  # Set the language
 
 # Connects to speech proxy to make the robot understand what a human says
 #understand_module = ALProxy("ALSpeechRecognition", IP, port)
@@ -46,7 +46,7 @@ vocabulary.append(keyword)
 wikipedia_mediawiki = MediaWiki()
 wikiPage = wikipedia_mediawiki.page(keyword)
 sections = wikiPage.sections
-print(sections)
+# print(sections)
 
 # Load and access data from full Wikipedia pages
 ny = wikipedia.page(keyword)
@@ -62,7 +62,7 @@ content = unicodedata.normalize('NFKD', wikipedia.summary(keyword, sentences=1))
 # Add the sections into the vocabulary
 vocabulary.extend(sections)  # We use extend to append a list to another list
 #understand_module.setVocabulary(vocabulary, False)
-speak_module.say(keyword)
+# speak_module.say(keyword)
 
 # Display the image on the tablet
 #tablet_module.showImage(imageStr)
@@ -70,65 +70,44 @@ speak_module.say(keyword)
 #tablet_module.hideImage()
 
 # Say the summary
-speak_module.say(content)
-
+# speak_module.say(content)
+print(content)
 # time.sleep(1)
 
 # Starting the recognizer
-rec = Recognizer(IP)
+# rec = Recognizer(IP)
 
 
 while True:
-    speak_module.say("Do you want more information?")
+    #speak_module.say("Do you want more information?")
+    print("Do you want more information?")
 
     # Faking the answer
-    user_input = "yes"
-    # user_input = "no"
+    user_input = raw_input()
 
     if user_input == "yes":
-        speak_module.say("Great! Which one of the following topic would you like to know more about?")
+        #speak_module.say("Great! Which one of the following topic would you like to know more about?")
+        print("Great! Which one of the following topic would you like to know more about?")
         for x in range(len(sections)/10):
-            print sections[x]
+            #speak_module.say(unicodedata.normalize('NFKD', sections[x]).encode('ascii', 'ignore'))
+            print(sections[x])
 
-            speak_module.say(unicodedata.normalize('NFKD', sections[x]).encode('ascii', 'ignore'))
+        while True:
+            # Faking the answer
+            user_input_section = raw_input()
 
-        # Faking the answer
-        user_input_section = "Education"
-
-        section_text = ny.section(user_input_section)
-
-        data = section_text.split(". ")
-
-
-        section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."
-
-        print(section_summary)
-
-        speak_module.say(unicodedata.normalize('NFKD', section_summary).encode('ascii', 'ignore'))
-
-        kappa = 0
-        speak_module.say("Do you want me to go on with this topic?")
-        # Faking the answer
-        user_input_continue_topic = "yes"
-        while user_input_continue_topic == "yes":
-
-            remaining_string = ""
-            kappa = kappa + 5
-            if(kappa > len(data)):
-                remaining = len(data) - (kappa - 5)
-                for x in remaining:
-                    remaining_string = remaining_string + data[x] + ". "
-                print("La fra puzza")
-                speak_module.say(unicodedata.normalize('NFKD', remaining_string).encode('ascii', 'ignore'))
+            if user_input_section in sections:
+                section_text = ny.section(user_input_section)
+                data = section_text.split(". ")
+                section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."
+                # speak_module.say(unicodedata.normalize('NFKD', section_summary).encode('ascii', 'ignore'))
+                print(section_summary)
                 break
             else:
-                section_summary = data[0 + kappa] + ". " + data[1 + kappa ] + ". " + data[2 + kappa] + ". " + data[3 + kappa] + ". " + data[4 + kappa] + "."
-                speak_module.say(unicodedata.normalize('NFKD', section_summary).encode('ascii', 'ignore'))
-                speak_module.say("Do you want me to go on with this topic?")
-                # Faking the answer
-                user_input_continue_topic = "yes"
-                break
-            break
+                # speak_module.say("I'm sorry I didn't get that. Please answer with just the name of the section.")
+                print("I'm sorry I didn't get that. Please answer with just the name of the section.")
+
+                pass
 
         break
 
@@ -137,7 +116,9 @@ while True:
         break
 
     else:
-        speak_module.say("I'm sorry I didn't get that. Please answer yes or no.")
+        #speak_module.say("I'm sorry I didn't get that. Please answer yes or no.")
+        print("I'm sorry I didn't get that. Please answer yes or no.")
+
         pass
 
 
