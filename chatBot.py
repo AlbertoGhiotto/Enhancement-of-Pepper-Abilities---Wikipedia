d49056bc2ded9e69d465bb20c0aefdcc61351845
Alberto <alberto.ghiotto@hotmail.it>
2020-04-03 19:06:12 +0200

    Added ability to extract keyword from complex sentence

--- chatBot.py
+++ chatBot.py
@@ -5,13 +5,24 @@ from mediawiki import MediaWiki
 from mediawiki import exceptions
 import time
 
+
+def checkAnswer(answer, acceptedAnswer):
+    #answers = answer.split(" ")
+    for i in range(len(acceptedAnswer)):
+       # for j in range(len(answers)):
+        if acceptedAnswer[i] in answer:
+            # if answers[j] == acceptedAnswer[i]:
+            return [True, acceptedAnswer[i]]
+    return [False, False]
+
+
 def answerQuestion(question, acceptedAnswer, model):        # print the question based on the model parameter
     print(question)
     if model == 1:                                          # prints the accepted answer when asking which section the user wants to talk about
         for x in range(len(acceptedAnswer)/10):
-            # speak_module.say(unicodedata.normalize('NFKD', sections[x]).encode('ascii', 'ignore'))
             print(sections[x])
         print("Another section")
+        acceptedAnswer.append("Another section")            # add this element to the accepted answer list in order to being able to detect it with the "checkAnswer" function
     elif model == 1.5:                                      # prints the accepted answer when asking which section the user wants to talk about
         for x in range(len(acceptedAnswer)):                # in this case we print every section
             # speak_module.say(unicodedata.normalize('NFKD', sections[x]).encode('ascii', 'ignore'))
@@ -19,9 +30,11 @@ def answerQuestion(question, acceptedAnswer, model):        # print the question
 
     while True:
         user_answer = raw_input()
-
-        if user_answer in acceptedAnswer or (user_answer == "Another section" and model != 2):               # if the user's answer is contained in the list of accepted answer
+        # print(acceptedAnswer)
+        if user_answer in acceptedAnswer:               # if the user's answer is contained in the list of accepted answer
             return user_answer                                                                               # simply return it to the main program
+        elif checkAnswer(user_answer, acceptedAnswer)[0]:
+            return checkAnswer(user_answer, acceptedAnswer)[1]
         else:
             print("Sorry! I didn't get that!")
             if model == 1:      # corresponds to sections
@@ -55,7 +68,8 @@ while True:
             wikiPage = wikipedia_mediawiki.page(keyword)
             break
         except:
-            print("The information you want are not available on wikipedia! Try with something else")
+            print("The information you want are not available on wikipedia! Try with something else!")
+            print("What to you want to know?")
             pass
 
     wikiPage = wikipedia_mediawiki.page(keyword)
@@ -68,12 +82,10 @@ while True:
 
     while True:
         user_input = answerQuestion("Do you want more information?", ["yes", "no"], 2)          # ask the question given in parameters
-
         if user_input == "yes":
-
             user_input_section = answerQuestion("Great! Which one of the following topic would you like to know more about?", sections, 1)
             while True:
-                if user_input_section in sections:
+                if user_input_section in sections and user_input_section != "Another section":
                     section_text = wikiPage.section(user_input_section)       # get the section's text
                     data = section_text.split(". ")                     # split the text every ". " character
                     section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."     # get the first five sentences
@@ -88,12 +100,12 @@ while True:
                         break                                           # get out of the while
 
                 elif user_input_section == "Another section":           # this prints all the section but by restarting the while it asks again the question
+                    sections.remove("Another section")                  # remove the element which was added for the use in "checkAnswer" function
                     user_input_section = answerQuestion("Here are all the sections. Which one are you interested in?", sections, 1.5)
                     pass                                                # restart the while
             break
 
         elif user_input == "no":
-            print("Ok! Però stai calmo :)")
             break
 
 
