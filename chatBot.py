867fba44563c958cea0e6115dc415f6daf505401
Alberto <alberto.ghiotto@hotmail.it>
2020-04-03 17:45:02 +0200

    Managed different type of questions

--- chatBot.py
+++ chatBot.py
@@ -3,7 +3,7 @@ import wikipedia
 import unicodedata               # To convert unicode (read from wikipedia) to string
 from mediawiki import MediaWiki
 from mediawiki import exceptions
-
+import time
 
 def answerQuestion(question, acceptedAnswer, model):        # print the question based on the model parameter
     print(question)
@@ -32,61 +32,76 @@ def answerQuestion(question, acceptedAnswer, model):        # print the question
                 pass
 
 
-# Manage the keyword
 while True:
-    keyword_sentence = raw_input()
+    # Manage the keyword
+    while True:
+        keyword_sentence = raw_input()
 
-    keyword_sentences = keyword_sentence.split("is ")
-    keyword_sentences = keyword_sentences[1].split("?")
+        if "is " in keyword_sentence:
+            keyword_sentences = keyword_sentence.split("is ")
+        elif "about " in keyword_sentence:
+            keyword_sentences = keyword_sentence.split("about ")
+        else:
+            print("I'm sorry I didn't get that! What to you want to know?")
+            continue
 
-    keyword = keyword_sentences[0]
+        keyword_sentences = keyword_sentences[1].split("?")
 
-    # Use MediaWiki API
-    wikipedia_mediawiki = MediaWiki()
-    try:
-        wikiPage = wikipedia_mediawiki.page(keyword)
-        break
-    except:
-        print("The information you want are not available on wikipedia! Try with something else")
-        pass
+        keyword = keyword_sentences[0]
 
-wikiPage = wikipedia_mediawiki.page(keyword)
-sections = wikiPage.sections
-content = wikiPage.summarize(sentences=1)
+        # Use MediaWiki API
+        wikipedia_mediawiki = MediaWiki()
+        try:
+            wikiPage = wikipedia_mediawiki.page(keyword)
+            break
+        except:
+            print("The information you want are not available on wikipedia! Try with something else")
+            pass
 
-# Say the summary
-print("Great! That's what I know about " + keyword + "!")
-print(content)
+    wikiPage = wikipedia_mediawiki.page(keyword)
+    sections = wikiPage.sections
+    content = wikiPage.summarize(sentences=1)
 
-while True:
-    user_input = answerQuestion("Do you want more information?", ["yes", "no"], 2)          # ask the question given in parameters
+    # Say the summary
+    print("Great! That's what I know about " + keyword + "!")
+    print(content)
 
-    if user_input == "yes":
+    while True:
+        user_input = answerQuestion("Do you want more information?", ["yes", "no"], 2)          # ask the question given in parameters
 
-        user_input_section = answerQuestion("Great! Which one of the following topic would you like to know more about?", sections, 1)
-        while True:
-            if user_input_section in sections:
-                section_text = wikiPage.section(user_input_section)       # get the section's text
-                data = section_text.split(". ")                     # split the text every ". " character
-                section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."     # get the first five sentences
+        if user_input == "yes":
 
-                print(section_summary)                              # print the first five sentences
-                user_input_section_another = answerQuestion("Do you want to know about another section?", ["yes", "no"], 2)
+            user_input_section = answerQuestion("Great! Which one of the following topic would you like to know more about?", sections, 1)
+            while True:
+                if user_input_section in sections:
+                    section_text = wikiPage.section(user_input_section)       # get the section's text
+                    data = section_text.split(". ")                     # split the text every ". " character
+                    section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."     # get the first five sentences
 
-                if user_input_section_another == "yes":
-                    user_input_section = answerQuestion("Which section do you want to know more about?", sections, 1)
-                    pass                                            # restart the while
-                elif user_input_section_another == "no":
-                    break                                           # get out of the while
+                    print(section_summary)                              # print the first five sentences
+                    user_input_section_another = answerQuestion("Do you want to know about another section?", ["yes", "no"], 2)
 
-            elif user_input_section == "Another section":           # this prints all the section but by restarting the while it asks again the question
-                user_input_section = answerQuestion("Here are all the sections. Which one are you interested in?", sections, 1.5)
-                pass                                                # restart the while
-        break
+                    if user_input_section_another == "yes":
+                        user_input_section = answerQuestion("Which section do you want to know more about?", sections, 1)
+                        pass                                            # restart the while
+                    elif user_input_section_another == "no":
+                        break                                           # get out of the while
 
-    elif user_input == "no":
-        print("Ok! Però stai calmo :)")
+                elif user_input_section == "Another section":           # this prints all the section but by restarting the while it asks again the question
+                    user_input_section = answerQuestion("Here are all the sections. Which one are you interested in?", sections, 1.5)
+                    pass                                                # restart the while
+            break
+
+        elif user_input == "no":
+            print("Ok! Però stai calmo :)")
+            break
+
 
     # We'll be here only if the user does not want to know more about the topic -> ask if the user wants some other topic
-    print("Do you want to know about something else?")
-    break                                                       # get out of the while
+    user_another_topic = answerQuestion("Do you want to know about something else?", ["yes", "no"], 2)
+    if user_another_topic == "yes":
+        print("What do you want to know?")
+        pass
+    elif user_another_topic == "no":
+        print("Ok! That's it for today, see you next time! Bye!")
+        break                                                       # get out of the while
