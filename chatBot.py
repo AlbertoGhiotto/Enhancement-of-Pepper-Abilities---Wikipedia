585af3fa2e7f606ea80f2e95e49019be05c471cd
unknown <francesca.canale.95@gmail.com>
2020-04-08 16:21:26 +0200

    Added some comments and three different way of presenting the content

--- chatBot.py
+++ chatBot.py
@@ -6,11 +6,14 @@ from mediawiki import MediaWiki
 import topicExtractor
 import textSummarizationMC
 
-def is_not_blank(s):
-    return bool(s and s.strip())
+
+def is_not_blank(str):
+    # Check if str is an empty string
+    return bool(str and str.strip())
+
 
 def parenthesesRemover(sentence):
-    # Returns a copy of 'sentence' with any parenthesized text removed. Nested parentheses are handled.
+    # Returns a copy of 'sentence' with any parenthesized text removed. Nested parentheses are handled
     result = ''
     paren_level = 0
     for ch in sentence:
@@ -20,39 +23,38 @@ def parenthesesRemover(sentence):
             paren_level -= 1
         elif not paren_level:
             result += ch
-
-    # unicodedata.normalize('NFKD', '—').encode('ascii', 'ignore')
-    # result = result.replace('\n', ' ')
-
     return result
 
 
-def checkAnswer(answer, acceptedAnswer):            # check is one of the accepted answer is contained in the user's answer
+def checkAnswer(answer, acceptedAnswer):
+    # Check if inside the user's answer there is one of the accepted answers
     for i in range(len(acceptedAnswer)):
         if acceptedAnswer[i] in answer:
             return [True, acceptedAnswer[i]]        # the answer was found, return it
     return [False, False]
 
 
-def answerQuestion(question, acceptedAnswer, model):        # print the question based on the model parameter
+def answerQuestion(question, acceptedAnswer, model):            # print the question based on the model parameter
+
     print(question)
-    if model == 1:                                          # prints the accepted answer when asking which section the user wants to talk about
-        for x in range(len(acceptedAnswer)/7):
-            # acceptedAnswer[x] = acceptedAnswer[x].lower()
+
+    if model == 1:                                              # limited number of sections case
+        for x in range(len(acceptedAnswer)/7):                  # prints the accepted answer when asking which section the user wants to talk about
             print(acceptedAnswer[x])
         print("another section")
-        acceptedAnswer.append("another section")            # add this element to the accepted answer list in order to being able to detect it with the "checkAnswer" function
-    elif model == 1.5:                                      # prints the accepted answer when asking which section the user wants to talk about
-        for x in range(len(acceptedAnswer)):                # in this case we print every section
+        acceptedAnswer.append("another section")                # add this element to the accepted answer list in order to being able to detect it with the "checkAnswer" function
+    elif model == 1.5:                                          # all sections case
+        for x in range(len(acceptedAnswer)):                    # prints the accepted answer when asking which section the user wants to talk about
             print(acceptedAnswer[x])
-    elif model == 3:                                        #
-        for x in range(len(acceptedAnswer)-1):
-            print(acceptedAnswer[x+1])
+    elif model == 3:                                            # suggestions case
+        for x in range(len(acceptedAnswer)-1):                  # prints the accepted answer when asking which related topic the user wants to talk about
+            print(acceptedAnswer[x+1])                          # not considering the first one because it corresponds to the original topic
+
     while True:
         user_answer = raw_input()
-        if user_answer in acceptedAnswer:                           # if the user's answer is contained in the list of accepted answer
-            return user_answer                                      # simply return it to the main program
-        elif checkAnswer(user_answer, acceptedAnswer)[0]:
+        if user_answer in acceptedAnswer:                       # if the user's answer is exactly one of the accepted answers
+            return user_answer                                  # simply return it to the main program
+        elif checkAnswer(user_answer, acceptedAnswer)[0]:       # if not, check if one of the accepted answers is contained in the user's answer
             return checkAnswer(user_answer, acceptedAnswer)[1]
         else:
             print("Sorry! I didn't get that!")
@@ -64,24 +66,25 @@ def answerQuestion(question, acceptedAnswer, model):        # print the question
                 print("Please answer with just the name of the suggestion")
 
 
-def keywordExtraction():
+def keywordExtraction():                                    # Extract the keyword from user's input
+
     while True:
         keyword_sentence = raw_input()
 
-        if "is " in keyword_sentence:
+        if "is " in keyword_sentence:                       # for questions like "Who is - ?"
             keyword_sentences = keyword_sentence.split("is ")
-        elif "are " in keyword_sentence:
+        elif "are " in keyword_sentence:                    # for questions like "Who are the - ?"
             keyword_sentences = keyword_sentence.split("are ")
-        elif "about " in keyword_sentence:
+        elif "about " in keyword_sentence:                  # for questions like "What do you know about - ?"
             keyword_sentences = keyword_sentence.split("about ")
-        else:
-            try:
+        else:                                               # if it is not one of the patterned questions
+            try:                                            # check if the question has a page on wikipedia (for example if the user inputs only the keyword)
                 wikipedia_mediawiki = MediaWiki()
-                wikiPage = wikipedia_mediawiki.page(keyword_sentence, auto_suggest=False)
+                wikiPage = wikipedia_mediawiki.page(keyword_sentence, auto_suggest=False)       # check without auto-suggest
                 return keyword_sentence
             except:
                 try:
-                    wikiPage = wikipedia_mediawiki.page(keyword_sentence)
+                    wikiPage = wikipedia_mediawiki.page(keyword_sentence)                       # check with auto-suggest
                     return keyword_sentence
                 except:
                     print("I'm sorry I didn't get that! What to you want to know?")
@@ -92,50 +95,52 @@ def keywordExtraction():
         return keyword_sentences[0]
 
 
-def presentSection(sections):
+def presentSection(sections):                           # Present the section chosen by the user
+
     user_input_section = answerQuestion("Great! Which one of the following topic would you like to know more about?", sections, 1)
     while True:
         if user_input_section in sections and user_input_section != "another section":
-            user_input_section = user_input_section.capitalize()    # set the first letter uppercase for correspondence with API's format
-            section_text = wikiPage.section(user_input_section)  # get the section's text
-            # data = section_text.split(". ")  # split the text every ". " character
-            # section_summary = data[0] + ". " + data[1] + ". " + data[2] + ". " + data[3] + ". " + data[4] + "."  # get the first five sentences
-            print(section_text)
-            section_text = parenthesesRemover(section_text)
-            if isinstance(section_text, str):
-                section_text = section_text.decode('unicode-escape')
+            user_input_section = user_input_section.capitalize()            # set the first letter uppercase for correspondence with API's format
+            section_text = wikiPage.section(user_input_section)             # get the section's text
+            section_text = parenthesesRemover(section_text)                 # remove the parenthesis from the text
+
+            if isinstance(section_text, str):                               # if the text is a string
+                section_text = section_text.decode('unicode-escape')        # convert it to unicode
 
             section_summary = textSummarizationMC.textSummarization((unicodedata.normalize('NFKD', section_text).encode('ascii', 'ignore')))
-            print(section_summary)  # print the first five sentences
-            print(section_text)
+            print(section_summary)                                          # print the summary obtained form textSummarization API
+            #print(section_text)
+
             user_input_section_another = answerQuestion("Do you want to know about another section?", ["yes", "no"], 2)
 
             if user_input_section_another == "yes":
                 user_input_section = answerQuestion("Which section do you want to know more about?", sections, 1)
-                pass  # restart the while
+                pass    # restart the while
             elif user_input_section_another == "no":
-                break  # get out of the while
+                break   # get out of the while
 
-        elif user_input_section == "another section":  # this prints all the section but by restarting the while it asks again the question
-            sections.remove("another section")  # remove the element which was added for the use in "checkAnswer" function
+        elif user_input_section == "another section":           # this prints all the section but by restarting the while it asks again the question
+            sections.remove("another section")                  # remove the element which was added for the use in "checkAnswer" function
             user_input_section = answerQuestion("Here are all the sections. Which one are you interested in?", sections, 1.5)
-            pass  # restart the while
+            pass     # restart the while
 
 
-def presentSuggestion(suggestions):
+def presentSuggestion(suggestions):                     # Present the related topic (suggestion) chosen by the user
+
     user_input_suggestion = answerQuestion("Great! Which one of the following related topic would you like to know more about?", suggestions, 3)
     while True:
         if user_input_suggestion in suggestions:
-            suggestedPage = wikipedia_mediawiki.page(user_input_suggestion)
-            content = suggestedPage.summarize(sentences=3)
+            suggestedPage = wikipedia_mediawiki.page(user_input_suggestion)     # serach on wikipedia the suggestion's page
+            content = suggestedPage.summarize(sentences=3)                      # summarize its content
             content = parenthesesRemover(content)
-            print(content)  # print the first sentence
+            print(content)                                                      # print the summary
             break
         else:
             user_input_suggestion = answerQuestion("I'm sorry I didn't get that, please answer with just the name of the related topic", suggestions, 3)
 
 
-def topicproposer(topic, type):
+def topicProposer(topic, type):                 # Propose a conversation topic based on the type of the extracted topics
+
     if (type == "City" or type == "Country" or type == "Adm1" or type == "Continent" or type == "GeoPoliticalEntity" or type == "Park" or type =="Location" or type == "NaturalReserve"):
         print("Have you ever been in " + topic + "?")
     elif (type == "FullName" or type == "FirstName"):
@@ -157,6 +162,7 @@ def topicproposer(topic, type):
 
 
 behaviour = 0
+presenter = 0
 
 knownTopics = ["City", "Country", "Adm1", "Continent", "GeoPoliticalEntity", "Park", "Location", "NaturalReserve",
                "University", "Game","SoftwareCompany", "MediaCompany", "RetailingCompany", "TechnologyEquipmentCompany",
@@ -183,32 +189,40 @@ while True:
                 print("What do you want to know?")
                 keyword = keywordExtraction()
 
-    sections = wikiPage.sections        # get the list of section
+    # Sections
+    sections = wikiPage.sections        # get the list of sections
 
     for x in range(len(sections)):      # detecting the empty sections
         if not is_not_blank(wikiPage.section(sections[x])):
             sections[x] = None
-
     sections = filter(None, sections)   # removing the empty sections
 
     for x in range(len(sections)):      # set the first letter lower case to being user-friendlier :) (actually for speech to text)
         sections[x] = sections[x].lower()
 
-    content = wikiPage.summarize(sentences=4)
-    content = parenthesesRemover(content)
-    suggestions = wikipedia_mediawiki.search(keyword, 5, False)
+    # Suggestions
+    suggestions = wikipedia_mediawiki.search(keyword, 5, False)     # get the related topics
     for x in range(len(suggestions)):
-        suggestions[x] = suggestions[x].lower()
+        suggestions[x] = suggestions[x].lower()                     # set the first letter lower case to being user-friendlier :) (actually for speech to text)
 
-    # Say the summary
-    print("Great! That's what I know about " + keyword + "!")
-    print(content)
+    # Content
+    content = wikiPage.summarize(sentences=4)                   # get the summary of the wikipedia page
+    content = parenthesesRemover(content)                       # remove the parenteses
+    if presenter == 0:
+        print("Great! That's what I know about " + keyword + "!")
+    elif presenter == 1:
+        print("Oh, I know something about this topic!")
+    elif presenter == 2:
+        print("I was just reading an article about " + keyword + " last day.")
+    print(content)                                              # say the summary
+    presenter = (presenter + 1) % 3                             # incrementing the presenter so that is not always the same
 
-    topics = topicExtractor.extractTopic(unicodedata.normalize('NFKD', content).encode('ascii', 'ignore'))
+    # Topic of conversation
+    topics = topicExtractor.extractTopic(unicodedata.normalize('NFKD', content).encode('ascii', 'ignore'))      # extracting the topics from the page's content
     # TODO: Manage case when the no entity is detected
 
     while True:
-        user_input = answerQuestion("Do you want more information?", ["yes", "no"], 2)          # ask the question given in parameters
+        user_input = answerQuestion("Do you want more information?", ["yes", "no"], 2)
         if user_input == "yes":
             if behaviour == 0:
                 presentSection(sections)
@@ -218,15 +232,15 @@ while True:
 
             elif behaviour == 2:
                 for index in range(len(topics)):
-                    if(topics[index][1] in knownTopics ):
-                        topicproposer(topics[index][0],topics[index][1])
-                        raw_input()
+                    if(topics[index][1] in knownTopics ):                   # check if in the extracted topic there is something known to ask
+                        topicProposer(topics[index][0],topics[index][1])    # [0] name of the topic, [1] type of the topic
+                        raw_input()                                         # answer of the user about the topic
                         print("Oh! That's very interesting!")
                         break
                 else:
-                    presentSuggestion(suggestions)
+                    presentSuggestion(suggestions)                          # if there is nothing known -> behaviour 0
 
-            behaviour = (behaviour + 1) % 3
+            behaviour = (behaviour + 1) % 3         # incrementing the behavoiur so that is not always the same
             break
 
         elif user_input == "no":
@@ -237,7 +251,7 @@ while True:
     user_another_topic = answerQuestion("Do you want to know about something else?", ["yes", "no"], 2)
     if user_another_topic == "yes":
         print("What do you want to know?")
-        pass
+        pass                # restart the first while (keywordextraction)
     elif user_another_topic == "no":
         print("Ok! That's it for today, see you next time! Bye!")
-        break                                                       # get out of the while
+        break               # get out of the while -> end of the program
